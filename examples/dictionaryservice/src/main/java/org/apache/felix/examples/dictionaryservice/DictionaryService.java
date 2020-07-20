6cd30bada14c88cfa486cbdd855e1bc40e69437d
Carsten Ziegeler <cziegeler@apache.org>
2020-07-20 12:18:21 +0200

    Update examples to latest parent pom, update some dependencies to newer versions

--- examples/dictionaryservice/src/main/java/org/apache/felix/examples/dictionaryservice/DictionaryService.java
+++ examples/dictionaryservice/src/main/java/org/apache/felix/examples/dictionaryservice/DictionaryService.java
@@ -20,17 +20,17 @@ package org.apache.felix.examples.dictionaryservice;
 /**
  * A simple service interface that defines a dictionary service. A dictionary
  * service simply verifies the existence of a word.
- * 
+ *
  * @author <a href="mailto:dev@felix.apache.org">Felix Project Team</a>
  */
 public interface DictionaryService
 {
     /**
      * Check for the existence of a word.
-     * 
+     *
      * @param word the word to be checked.
      * @return true if the word is in the dictionary, false otherwise.
      */
-    public boolean checkWord( String word );
+    boolean checkWord( String word );
 
 }
--- examples/dictionaryservice/src/main/java/org/apache/felix/examples/dictionaryservice/impl/Activator.java
+++ examples/dictionaryservice/src/main/java/org/apache/felix/examples/dictionaryservice/impl/Activator.java
@@ -17,7 +17,8 @@
 package org.apache.felix.examples.dictionaryservice.impl;
 
 
-import java.util.Properties;
+import java.util.Dictionary;
+import java.util.Hashtable;
 
 import org.apache.felix.examples.dictionaryservice.DictionaryService;
 import org.osgi.framework.BundleActivator;
@@ -29,7 +30,7 @@ import org.osgi.framework.BundleContext;
  * register an English language dictionary service with the OSGi framework. The
  * dictionary service interface is defined in a separate class file and is
  * implemented by an inner class.
- * 
+ *
  * @author <a href="mailto:dev@felix.apache.org">Felix Project Team</a>
  */
 public class Activator implements BundleActivator
@@ -38,29 +39,31 @@ public class Activator implements BundleActivator
      * Implements BundleActivator.start(). Registers an instance of a dictionary
      * service using the bundle context; attaches properties to the service that
      * can be queried when performing a service look-up.
-     * 
+     *
      * @param context the framework context for the bundle.
      */
+    @Override
     public void start( BundleContext context )
     {
-        Properties props = new Properties();
+        Dictionary<String, Object> props = new Hashtable<>();
         props.put( "Language", "English" );
-        context.registerService( DictionaryService.class.getName(), new DictionaryImpl(), props );
+        context.registerService( DictionaryService.class, new DictionaryImpl(), props );
     }
 
 
     /**
      * Implements BundleActivator.stop(). Does nothing since the framework will
      * automatically unregister any registered services.
-     * 
+     *
      * @param context the framework context for the bundle.
      */
+    @Override
     public void stop( BundleContext context )
     {
         // NOTE: The service is automatically unregistered.
     }
 
-    
+
     /**
      * A private inner class that implements a dictionary service; see
      * DictionaryService for details of the service.
@@ -73,10 +76,11 @@ public class Activator implements BundleActivator
         /**
          * Implements DictionaryService.checkWord(). Determines if the passed in
          * word is contained in the dictionary.
-         * 
+         *
          * @param word the word to be checked.
          * @return true if the word is in the dictionary, false otherwise.
          */
+        @Override
         public boolean checkWord( String word )
         {
             word = word.toLowerCase();
