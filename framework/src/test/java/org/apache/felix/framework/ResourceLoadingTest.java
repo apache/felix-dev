80ac5ea77b1935058b4408e34d162ca2cd59bad4
Karl Pauls <kpauls@adobe.com>
2020-09-07 12:42:29 +0200

    Improve a test case

--- framework/src/test/java/org/apache/felix/framework/ResourceLoadingTest.java
+++ framework/src/test/java/org/apache/felix/framework/ResourceLoadingTest.java
@@ -34,6 +34,8 @@ import junit.framework.TestCase;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.Constants;
 import org.osgi.framework.launch.Framework;
+import org.osgi.framework.wiring.BundleRevision;
+import org.osgi.framework.wiring.BundleWiring;
 
 public class ResourceLoadingTest extends TestCase
 {
@@ -105,6 +107,12 @@ public class ResourceLoadingTest extends TestCase
         assertEquals("This is a Test", reader.readLine());
 
         reader = new BufferedReader(new InputStreamReader(testBundle.getEntry(name).openStream()));
+        assertEquals("This is a Test", reader.readLine());
+
+        reader = new BufferedReader(new InputStreamReader(testBundle.adapt(BundleWiring.class).getClassLoader().getResourceAsStream(name)));
+        assertEquals("This is a Test", reader.readLine());
+
+        reader = new BufferedReader(new InputStreamReader(testBundle.adapt(BundleWiring.class).getClassLoader().getResource(name).openStream()));
         assertEquals("This is a Test", reader.readLine());
     }
 
