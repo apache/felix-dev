d4282bb99ec28549d2de7314aab5e406dc208a74
Karl Pauls <pauls@apache.org>
2021-05-26 16:32:12 +0200

    Merge pull request #77 from apache/issues/FELIX-6416
    FELIX-6416: special case runtime ref in bundle urls

--- framework/src/test/java/org/apache/felix/framework/ResourceLoadingTest.java
+++ framework/src/test/java/org/apache/felix/framework/ResourceLoadingTest.java
@@ -20,12 +20,17 @@ package org.apache.felix.framework;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.jar.JarOutputStream;
 import java.util.jar.Manifest;
@@ -122,6 +127,59 @@ public class ResourceLoadingTest extends TestCase
 
         reader = new BufferedReader(new InputStreamReader(testURL.openStream()));
         assertEquals("This is a Test", reader.readLine());
+    }
+
+    public void testResourceLoadingUsingURLClassLoaderJDK9() throws Exception {
+        String bmf = "Bundle-SymbolicName: cap.bundle\n"
+            + "Bundle-Version: 1.2.3.Blah\n"
+            + "Bundle-ManifestVersion: 2\n"
+            + "Import-Package: org.osgi.framework\n";
+        File bundleFile = File.createTempFile("felix-bundle", ".jar", tempDir);
+        ByteArrayOutputStream embeddedJar1 = new ByteArrayOutputStream();
+        ByteArrayOutputStream embeddedJar2 = new ByteArrayOutputStream();
+
+        Manifest mf = new Manifest(new ByteArrayInputStream(bmf.getBytes("utf-8")));
+        mf.getMainAttributes().putValue("Manifest-Version", "1.0");
+        JarOutputStream bundle1 = new JarOutputStream(new FileOutputStream(bundleFile), mf);
+        JarOutputStream ej1 = new JarOutputStream(embeddedJar1, mf);
+        JarOutputStream ej2 = new JarOutputStream(embeddedJar2, mf);
+
+        String ej1Entry = "ej1.txt";
+        ej1.putNextEntry(new ZipEntry(ej1Entry));
+        ej1.write("This is a Test".getBytes());
+        ej1.close();
+
+        String ej2Entry = "ej2.txt";
+        ej2.putNextEntry(new ZipEntry(ej2Entry));
+        ej2.write("This is a Test".getBytes());
+        ej2.close();
+
+        String bundleResource = "b1.txt";
+        bundle1.putNextEntry(new ZipEntry(bundleResource));
+        bundle1.write("This is a Test".getBytes());
+        bundle1.putNextEntry(new ZipEntry("ej1.jar"));
+        bundle1.write(embeddedJar1.toByteArray());
+        bundle1.putNextEntry(new ZipEntry("ej2.jar"));
+        bundle1.write(embeddedJar2.toByteArray());
+        bundle1.close();
+
+        Bundle testBundle = felix.getBundleContext().installBundle(bundleFile.toURI().toASCIIString());
+
+        testBundle.start();
+
+        ClassLoader urlClassLoader = createClassLoader(testBundle);
+
+        assertNotNull(urlClassLoader.getResource("ej2.txt"));
+    }
+
+    ClassLoader createClassLoader(Bundle bundle) {
+        List<URL> urls = new ArrayList<URL>();
+        Collection<String> resources = bundle.adapt(BundleWiring.class).listResources("/", "*.jar", BundleWiring.LISTRESOURCES_LOCAL);
+        for (String resource : resources) {
+            urls.add(bundle.getResource(resource));
+        }
+        // Create the classloader
+        return new URLClassLoader(urls.toArray(new URL[urls.size()]), getClass().getClassLoader());
     }
 
     private static void deleteDir(File root) throws IOException
