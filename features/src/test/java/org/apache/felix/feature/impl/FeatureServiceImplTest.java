1573d10023d63f26fcfd65aadc71b02b1b36bfd1
Carsten Ziegeler <cziegeler@apache.org>
2022-11-04 07:46:02 +0100

    FELIX-6579 : Migrate to jakarta json

--- features/src/test/java/org/apache/felix/feature/impl/FeatureServiceImplTest.java
+++ features/src/test/java/org/apache/felix/feature/impl/FeatureServiceImplTest.java
@@ -34,9 +34,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonReader;
 
 import org.junit.Before;
 import org.junit.Test;
