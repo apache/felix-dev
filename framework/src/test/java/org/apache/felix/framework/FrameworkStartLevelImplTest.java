bbd23a5b5462ae9b6ffe059645e48c834a9acaff
Jochen Hiller <jochen.hiller@telekom.de>
2022-12-12 14:57:31 +0100

    Added Apache License header, changed default start level to 12 to indicate it does not need to be 10+1

--- framework/src/test/java/org/apache/felix/framework/FrameworkStartLevelImplTest.java
+++ framework/src/test/java/org/apache/felix/framework/FrameworkStartLevelImplTest.java
@@ -1,17 +1,21 @@
 /*
-How to test:
-
-git clone -b feature/test-startlevel-impl https://github.com/JochenHiller/felix-dev
-cd felix-dev
-cd framework
-mvn clean compile
-mvn test -Dtest=FrameworkStartLevelImplTest
-
-See as well:
-* https://issues.apache.org/jira/browse/FELIX-6586
-
-*/
-
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.felix.framework;
 
 import java.io.ByteArrayInputStream;
@@ -91,7 +95,7 @@ public class FrameworkStartLevelImplTest extends TestCase {
      * start order should be C, B, A.
      */
     public void testStartLevelManipulatedByBundle() throws Exception {
-        int initialBundleStartLevel = 11; // 11, 21, 31 does fail, >40 does work
+        int initialBundleStartLevel = 12; // 12, 25, 37 does fail, >40 does work
 
         File tmpDir = createTmpDir("generated-bundles");
         File cacheDir = createTmpDir("felix-cache");
