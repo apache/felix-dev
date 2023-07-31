8604ed7931083bd197eedeb86bd54495f72c1c3f
Carsten Ziegeler <cziegeler@apache.org>
2023-07-31 07:17:21 +0200

    FELIX-6621 : Regressions caused by FELIX-6607

--- src/main/java/org/apache/felix/webconsole/internal/servlet/Plugin.java
+++ src/main/java/org/apache/felix/webconsole/internal/servlet/Plugin.java
@@ -20,6 +20,7 @@ package org.apache.felix.webconsole.internal.servlet;
 
 import java.util.Collections;
 import java.util.Enumeration;
+import java.util.NoSuchElementException;
 
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
@@ -27,9 +28,14 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
 import org.apache.felix.webconsole.AbstractWebConsolePlugin;
+import org.apache.felix.webconsole.WebConsoleConstants;
+import org.apache.felix.webconsole.internal.OsgiManagerPlugin;
+import org.apache.felix.webconsole.internal.Util;
+import org.apache.felix.webconsole.internal.WebConsolePluginAdapter;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceReference;
+import org.osgi.service.log.LogService;
 
 public abstract class Plugin implements ServletConfig, Comparable<Plugin> {
 
@@ -38,7 +44,9 @@ public abstract class Plugin implements ServletConfig, Comparable<Plugin> {
 
     private final ServiceReference<Servlet> serviceReference; // used for comparing conflicting services
 
-    private volatile String title;
+    protected volatile String title;
+
+    protected volatile String category;
 
     private volatile AbstractWebConsolePlugin consolePlugin;
 
@@ -52,13 +60,27 @@ public abstract class Plugin implements ServletConfig, Comparable<Plugin> {
         return this.serviceReference;
     }
 
-    public abstract String getId();
-
     public Bundle getBundle() {
         if ( this.serviceReference != null ) {
             return this.serviceReference.getBundle();
         }
         return this.holder.getBundleContext().getBundle();
+    }
+
+    /**
+     * Initialize everything including title and category
+     */
+    public boolean init() {
+        final AbstractWebConsolePlugin plugin = this.doGetConsolePlugin();
+        if ( plugin != null ) {
+            try {
+                plugin.init(this);
+            } catch (final ServletException e) {
+                // ignore this
+            }
+            this.consolePlugin = plugin;
+        }
+        return this.getConsolePlugin() != null;
     }
 
     /**
@@ -90,73 +112,50 @@ public abstract class Plugin implements ServletConfig, Comparable<Plugin> {
 
     @Override
     public int compareTo(final Plugin other) {
-        // serviceReference = null means internal (i.e. service.ranking=0 and service.id=0)
-        final Long id = serviceReference != null ? (Long) serviceReference.getProperty(Constants.SERVICE_ID) : 0;
-        final Long otherId = other.serviceReference != null ? (Long) other.serviceReference.getProperty(Constants.SERVICE_ID) : 0;
-        if (id.compareTo(otherId) == 0) {
-            return 0; // same service or both internal
+        int result = this.getLabel().compareTo(other.getLabel());
+        if (result == 0) {
+            // serviceReference = null means internal (i.e. service.ranking=0 and service.id=0)
+            final Long id = serviceReference != null ? (Long) serviceReference.getProperty(Constants.SERVICE_ID) : 0;
+            final Long otherId = other.serviceReference != null ? (Long) other.serviceReference.getProperty(Constants.SERVICE_ID) : 0;
+            result = id.compareTo(otherId);
+            if (result != 0) { // same service or both internal if 0
+                final Integer rank = getRanking(this.serviceReference);
+                final Integer otherRank = getRanking(other.serviceReference);
+                result = rank.compareTo(otherRank);
+                if (result == 0) {
+                    // If ranks are equal, then sort by service id in descending order.
+                    result = -1 * id.compareTo(otherId);
+                }
+            }
         }
-
-        final Integer rank = getRanking(this.serviceReference);
-        final Integer otherRank = getRanking(other.serviceReference);
-
-        // Sort by rank in ascending order.
-        if (rank.compareTo(otherRank) < 0) {
-            return -1; // lower rank
-        } else if (rank.compareTo(otherRank) > 0) {
-            return 1; // higher rank
-        }
-
-        // If ranks are equal, then sort by service id in descending order.
-        return -1 * id.compareTo(otherId);
-    }
-
-    protected PluginHolder getHolder() {
-        return holder;
+        return result;
     }
 
     public String getLabel() {
         return label;
     }
 
-    protected void setTitle(final String title ) {
-        this.title = title;
+    protected PluginHolder getHolder() {
+        return holder;
     }
 
     public String getTitle() {
         return this.title != null ? this.title : this.getLabel();
     }
 
-    // methods added to support categories
-
-    final String getCategory() {
-        return doGetCategory();
+    public String getCategory() {
+        return this.category;
     }
 
-    protected String doGetCategory() {
-        // get the service now
-        final AbstractWebConsolePlugin plugin = getConsolePlugin();
-        return ( plugin != null ) ? plugin.getCategory() : null;
-    }
-
-    public final AbstractWebConsolePlugin getConsolePlugin() {
-        if ( this.consolePlugin == null ) {
-            final AbstractWebConsolePlugin plugin = this.doGetConsolePlugin();
-            if ( plugin != null ) {
-                try {
-                    plugin.init(this);
-                } catch (final ServletException e) {
-                    // ignore this
-                }
-                this.consolePlugin = plugin;
-            }
-        }
+    public AbstractWebConsolePlugin getConsolePlugin() {
         return this.consolePlugin;
     }
 
     protected boolean isEnabled() {
         return true;
     }
+
+    public abstract String getId();
 
     protected abstract AbstractWebConsolePlugin doGetConsolePlugin();
 
@@ -182,5 +181,136 @@ public abstract class Plugin implements ServletConfig, Comparable<Plugin> {
     @Override
     public String getServletName() {
         return getTitle();
+    }
+
+    public static class ServletPlugin extends Plugin {
+
+        public ServletPlugin( final PluginHolder holder, final ServiceReference<Servlet> serviceReference, final String label ) {
+            super(holder, serviceReference, label);
+        }
+
+        public String getId() {
+            return this.getServiceReference().toString();
+        }
+
+        protected AbstractWebConsolePlugin doGetConsolePlugin() {
+            final Servlet service = getHolder().getBundleContext().getService( this.getServiceReference() );
+            if ( service != null ) {
+                this.title = Util.getStringProperty( this.getServiceReference(), WebConsoleConstants.PLUGIN_TITLE );
+                this.category = Util.getStringProperty( this.getServiceReference(), WebConsoleConstants.PLUGIN_CATEGORY );
+                final AbstractWebConsolePlugin servlet;
+                if ( service instanceof AbstractWebConsolePlugin ) {
+                    servlet = ( AbstractWebConsolePlugin ) service;
+                    if (this.title == null) {
+                        this.title = servlet.getTitle();
+                    }
+                    if (this.category == null) {
+                        this.category = servlet.getCategory();
+                    }
+                } else {
+                    servlet = new WebConsolePluginAdapter( getLabel(), service, this.getServiceReference() );
+                }
+                return servlet;
+            }
+            return null;
+        }
+
+        protected void doUngetConsolePlugin( AbstractWebConsolePlugin consolePlugin ) {
+            try {
+                getHolder().getBundleContext().ungetService( this.getServiceReference() );
+            } catch ( final IllegalStateException ise ) {
+                // ignore - bundle context is no longer valid
+            }
+        }
+
+        //---------- ServletConfig overwrite (based on ServletReference)
+
+        @Override
+        public String getInitParameter( final String name ) {
+            Object property = this.getServiceReference().getProperty( name );
+            if ( property != null && !property.getClass().isArray() ) {
+                return property.toString();
+            }
+
+            return super.getInitParameter( name );
+        }
+
+        @Override
+        public Enumeration<?> getInitParameterNames() {
+            final String[] keys = this.getServiceReference().getPropertyKeys();
+            return new Enumeration<Object>() {
+                int idx = 0;
+
+                public boolean hasMoreElements() {
+                    return idx < keys.length;
+                }
+
+                public Object nextElement() {
+                    if ( hasMoreElements() ) {
+                        return keys[idx++];
+                    }
+                    throw new NoSuchElementException();
+                }
+            };
+        }
+    }
+
+    public static class InternalPlugin extends Plugin {
+
+        private final String pluginClassName;
+        private final OsgiManager osgiManager;
+        private volatile boolean doLog = true;
+
+        public InternalPlugin(PluginHolder holder, OsgiManager osgiManager, String pluginClassName, String label) {
+            super(holder, null, label);
+            this.osgiManager = osgiManager;
+            this.pluginClassName = pluginClassName;
+        }
+
+        public String getId() {
+            return this.pluginClassName;
+        }
+
+        protected final boolean isEnabled() {
+            // check if the plugin is enabled
+            return !osgiManager.isPluginDisabled(pluginClassName);
+        }
+
+        protected AbstractWebConsolePlugin doGetConsolePlugin() {
+            if (!isEnabled()) {
+                if (doLog) {
+                    osgiManager.log( LogService.LOG_INFO, "Ignoring plugin " + pluginClassName + ": Disabled by configuration" );
+                    doLog = false;
+                }
+                return null;
+            }
+
+            AbstractWebConsolePlugin plugin = null;
+            try {
+                Class<?> pluginClass = getClass().getClassLoader().loadClass(pluginClassName);
+                plugin = (AbstractWebConsolePlugin) pluginClass.getDeclaredConstructor().newInstance();
+
+                if (plugin instanceof OsgiManagerPlugin) {
+                    ((OsgiManagerPlugin) plugin).activate(osgiManager.getBundleContext());
+                }
+                this.title = plugin.getTitle();
+                this.category = plugin.getCategory();
+                doLog = true; // reset logging if it succeeded
+            } catch (final Throwable t) {
+                plugin = null; // in case only activate has faled!
+                if (doLog) {
+                    osgiManager.log( LogService.LOG_WARNING, "Failed to instantiate plugin " + pluginClassName, t );
+                    doLog = false;
+                }
+            }
+
+            return plugin;
+        }
+
+        protected void doUngetConsolePlugin(final AbstractWebConsolePlugin plugin) {
+            if (plugin instanceof OsgiManagerPlugin) {
+                ((OsgiManagerPlugin) plugin).deactivate();
+            }
+        }
     }
 }
