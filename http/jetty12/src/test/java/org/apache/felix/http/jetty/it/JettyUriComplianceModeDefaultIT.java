473b16c764a6f3d90c755bd207eef0eac71cf019
Paul <paul@blueconic.com>
2024-05-08 09:45:13 +0200

    Update JettyUriComplianceModeDefaultIT.java

--- http/jetty12/src/test/java/org/apache/felix/http/jetty/it/JettyUriComplianceModeDefaultIT.java
+++ http/jetty12/src/test/java/org/apache/felix/http/jetty/it/JettyUriComplianceModeDefaultIT.java
@@ -54,7 +54,7 @@ public class JettyUriComplianceModeDefaultIT extends AbstractJettyTestSupport {
 
     @Override
     protected Option[] additionalOptions() throws IOException {
-        String jettyVersion = System.getProperty("jetty.version", "12.0.8");
+        String jettyVersion = System.getProperty("jetty.version", "12.0.9");
         return new Option[] {
                 spifly(),
 
