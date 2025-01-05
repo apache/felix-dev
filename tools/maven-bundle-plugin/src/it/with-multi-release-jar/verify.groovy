a18314f1cefc2537f90b774b80f517e5c85402e1
Paul Rütter <paul@blueconic.com>
2025-01-05 07:40:47 +0100

    FELIX-6741-Upgrade-dependencies-for-maven-bundle-plugin - Reset maven version to 3.3.9, as specified in bnd 7.1.0 changelog - Fix tests; add build timestamp greater than 1980 (required in new maven-archiver) - Fix tests; change bnd version

--- tools/maven-bundle-plugin/src/it/with-multi-release-jar/verify.groovy
+++ tools/maven-bundle-plugin/src/it/with-multi-release-jar/verify.groovy
@@ -20,7 +20,7 @@ String manifest = new File( basedir, "target/classes/META-INF/MANIFEST.MF" ).tex
 assert !manifest.isEmpty()
 
 manifest.eachLine() { line ->
-    if (line.contains("Tool") && !line.contains("7.0.0")) {
+    if (line.contains("Tool") && !line.contains("7.1.0")) {
         throw new Exception("Wrong bnd version used");
     }
     if (line.contains("Embedded-Artifacts") && !line.contains("jersey-server-3.1.7.jar")) {
